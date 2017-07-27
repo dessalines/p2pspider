dce7cf170e27114b83267e0786fc4fc8737f6882
Tyler <happydooby@gmail.com>
2017-07-27 15:47:37 -0700

    a

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ p2p.on('metadata', function (metadata) {
     var data = bencode.decode(bcode, 'utf8').info;
     console.log(data);
 
-    var bytes = (data.files.length) ? Object.keys(data.files).reduce((acc, value) => acc + obj['length'], 0) : data['length'];
+    var bytes = (data.files.length) ? data.files.reduce((a, b) => a + b['length'], 0) : data['length'];
     const stmt = 'insert into torrent (info_hash, name, size_bytes, bencode) values ($1, $2, $3, $4)';
     const values = [metadata.infohash, data.name, bytes, bcode];
 
